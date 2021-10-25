9339c3a3a672b8bc8ddbc0aa11cb1d824b50a8c9
nightcrawler123 <nightcrawler123@users.noreply.github.com>
2021-10-26 02:31:27 +0530

    File update

--- _inc/loader.php
+++ _inc/loader.php
@@ -1,4 +1,4 @@
 <?php
 
 	require_once plugin_dir_path( __FILE__ ) . 'core.class.php';
-	require_once plugin_dir_path( __FILE__ ) . 'salter.class.php';
+	require_once plugin_dir_path( __FILE__ ) . 'saltpepper.class.php';
